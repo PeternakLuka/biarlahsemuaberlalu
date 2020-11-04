45d6d225312ef33479616c95361557f72dc54019
Rizky Galuh <43767902+PeternakLuka@users.noreply.github.com>
2020-11-04 10:49:57 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -27,17 +27,23 @@
 
             .grid-container1 > div {
                 background-color: rgba(255, 255, 255, 0.8);
-                text-align: center;
+                /* text-align: center; */
                 font-size: 20px;
             }
 
             .grid-container2 {
                 display: grid;
-                grid-template-columns: auto auto auto auto;
+                grid-template-columns: auto auto auto;
             }
         </style>
     </head>
     <body>
+        <?php
+            include_once("include/koneksi.php");
+            $result = mysqli_query($conn, "SELECT * FROM tb_data ORDER BY konfirmasi ASC LIMIT 20");
+            $result2 = mysqli_query($conn, "SELECT * FROM tb_data ORDER BY konfirmasi ASC LIMIT 21,20");
+            $result_img = mysqli_query($conn, "SELECT imageId FROM tb_foto");
+        ?>
         <table width="100%">
             <tr>
                 <td align="left">Header Kiri</td>
@@ -51,79 +57,65 @@
             <div class="mantap4">Malang Raya</div>
         </div>
         <div class="grid-container1">
-            <div class="461">DATA COVID-19 PROVINSI JATIM</div>
-            <div class="462">DATA COVID-19 KOTA MALANG</div>
+            <div class="461" style="text-align: center">DATA COVID-19 PROVINSI JATIM</div>
+            <div class="462" style="text-align: right">DATA COVID-19 KOTA MALANG</div>
         </div>
         <div class="grid-container2">
+            <?php $d = 0 ?>
             <div class="patnam1">
-                <table border="1">
+                <table border="1" width="90%">
                     <tr>
                         <td>Kota/Kabupaten</td>
                         <td>Konfirmasi</td>
                         <td>Sembuh</td>
                         <td>Suspect</td>
                     </tr>
+                    <?php  
+                        while($data = mysqli_fetch_array($result)) {   
+                            echo "<tr>";
+                            echo "<td>".$data['ktkb']."</td>";
+                            echo "<td>".$data['konfirmasi']."</td>";
+                            echo "<td>".$data['sembuh']."</td>";
+                            echo "<td>".$data['suspek']."</td>";  
+                            echo "</tr>"; 
+                            $d++;
+                        }
+                    ?>
                 </table>
             </div>
             <div class="patnam2">
-                <table border="1">
+                <table border="1" width="90%" >
                     <tr>
                         <td>Kota/Kabupaten</td>
                         <td>Konfirmasi</td>
                         <td>Sembuh</td>
                         <td>Suspect</td>
                     </tr>
+                    <?php  
+                        while($data = mysqli_fetch_array($result2)) {   
+                            echo "<tr>";
+                            echo "<td>".$data['ktkb']."</td>";
+                            echo "<td>".$data['konfirmasi']."</td>";
+                            echo "<td>".$data['sembuh']."</td>";
+                            echo "<td>".$data['suspek']."</td>";  
+                            echo "</tr>"; 
+                            $d++;
+                        }
+                    ?>
                 </table>
             </div>
-            <div class="patnam3">Foto Col 1<br/>Row 1 & 2</div>  
-            <div class="patnam4">Foto Col 2<br/>Row 1 & 2</div>
+            <div class="patnam3">
+                <?php
+                    while($row = mysqli_fetch_array($result_img)) {
+                    ?>
+                        <img src="imageView.php?image_id=<?php echo $row["imageId"]; ?>" width="100px" height="100px" /><br/>
+                    
+                <?php		
+                    }
+                    mysqli_close($conn);
+                ?>
+            </div>  
+            <!-- <div class="patnam4">Foto Col 2<br/>Row 1 & 2</div> -->
         </div>
     </body>
 </html>
-
-
-<!-- <title>
-    Update Percepatan Virus COVID-19 Kota Malang
-</title>
-<body>
-    <table width="100%">
-        <tr>
-            <td colspan="2">Header Kiri</td>
-            <td colspan="2" align="right">Header Kanan</td>
-        </tr>
-        <tr>
-            <td align="center">Positif</td>
-            <td align="center">Sembuh</td>
-            <td align="center">Meninggal</td>
-            <td align="center">Malang Raya</td>
-        </tr>
-        <tr>
-            <td><br/></td>
-            <table width="100%">
-                <tr>
-                    <td colspan="2" align="center"><strong>DATA COVID-19 PROVINSI JATIM</strong></td>
-                    <td colspan="2" align="center"><strong>DATA COVID-19 KOTA MALANG</strong></td>
-                </tr>
-                <tr>
-                    <td>
-                        <tr>
-                            <td>KOTA/KABUPATEN</td>
-                            <td>KONFIRMASI</td>
-                            <td>SEMBUH</td>
-                            <td>SUSPECT</td>
-                        </tr>
-                    </td>
-                    <td>
-                        KOTA/KABUPATEN 2
-                    </td>
-                    <td>
-                        Foto 1
-                    </td>
-                    <td>
-                        FOTO 2
-                    </td>
-                </tr>
-            </table>
-        </tr>
-    </table>
-</body> -->
